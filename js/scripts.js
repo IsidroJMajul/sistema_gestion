29452e8025d1038498534698ad24b25fbb47c6ac
Isidro <isidromajul@hotmail.com>
2024-08-07 18:31:40 -0300

    -

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Conexión a base de datos MySQL
+
+let mysql = require('mysql');
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "users_gestion"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.end()
+});
+
+con.connect(function(err) {
+    // if (err) throw err;
+    con.query("SELECT * FROM users", function (err, result, fields) {
+    //   if (err) throw err;
+      console.log(result);
+    });
+  });
+
+/* PROXIMO PASO:
+El usuario inserte los datos en los campos inputs y aprete el botón de INGRESAR
+Al presionar el botón INGRESAR, el script deberá:
+. conectarse a la base de datos
+. buscar la información colocada en los inputs (dni y password)
+. en caso de NO encontrar la información, aparecerá un alerta de ERROR y cerrará la conexión.
+. en caso de SÍ encontrar la información, se dará ingreso a la plataforma y se cargará la siguiente página (por el momento, sólo marcará un alerta de ingreso correcto y cerrará la conexión)
+*/
