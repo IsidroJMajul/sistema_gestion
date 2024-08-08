742fe63b54ec801651a101d62c3ef643b369d4a6
Isidro <isidromajul@hotmail.com>
2024-08-08 11:03:53 -0300

    -

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,20 @@ con.connect(function(err) {
       console.log(result);
     });
   });
-
+  
 /* PROXIMO PASO:
 El usuario inserte los datos en los campos inputs y aprete el botón de INGRESAR
 Al presionar el botón INGRESAR, el script deberá:
 . conectarse a la base de datos
-. buscar la información colocada en los inputs (dni y password)
+. buscar la información colocada en los inputs (dni y password) OK
 . en caso de NO encontrar la información, aparecerá un alerta de ERROR y cerrará la conexión.
 . en caso de SÍ encontrar la información, se dará ingreso a la plataforma y se cargará la siguiente página (por el momento, sólo marcará un alerta de ingreso correcto y cerrará la conexión)
 */
+
+function valorDniPass() {
+  let dni = document.getElementById('dni_input');
+  let dniValue = dni.value;
+  let pass = document.getElementById('pass_input');
+  let passValue = pass.value;
+  alert("DNI: " + dniValue + " " + "Password: " + passValue)
+}
